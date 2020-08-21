f8a9339f2d7c0908585abb86bbe26a6721400427
HubTicaBB <tijana.ilic87@protonmail.com>
2020-08-21 10:48:54 +0200

    Replace anchors by Links and NavLinks

--- client/src/components/AboutPage.js
+++ client/src/components/AboutPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const AboutPage = () => {
     return (
         <>
             <h1>About</h1>
             <p>This app uses React.</p>
-            <a href='/' className='btn btn-primary'>Back to Home Page</a>
+            <Link to='/' className='btn btn-primary'>Back to Home Page</Link>
         </>
     );
 };
--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
     return (
         <div className='jumbotron'>
             <h1>Pluralsight Administration</h1>
             <p>React, Fux, and React Router for ultra-responsive web apps.</p>
-            <a href='/about' className='btn btn-primary'>About</a>
+            <Link to='/about' className='btn btn-primary'>About</Link>
         </div>
     );
 };
