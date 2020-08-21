7b6508d8cf4f6eeeac45d5f0469f2276c028ec72
HubTicaBB <tijana.ilic87@protonmail.com>
2020-08-21 09:00:16 +0200

    Create navbar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import Header from './common/Header';
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 
 const App = () => {
-    const route = window.location.pathname;
-    if (route === '/about') return <AboutPage />
-    return <HomePage />
+    const getPage = () => {
+        const route = window.location.pathname;
+        if (route === '/about') return <AboutPage />
+        return <HomePage />
+    };
+
+    return (
+        <div className='container-fluid'>
+            <Header />
+            { getPage() }
+        </div>
+    );
 };
 
 export default App;
