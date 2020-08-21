03d3e4be208d5afb35df480dc2812b3d21d6d54c
HubTicaBB <tijana.ilic87@protonmail.com>
2020-08-21 09:42:10 +0200

    Create CoursesPage class component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './common/Header';
 import HomePage from './HomePage';
+import CoursesPage from './CoursesPage'
 import AboutPage from './AboutPage';
 
 const App = () => {
     const getPage = () => {
         const route = window.location.pathname;
-        if (route === '/about') return <AboutPage />
+        if (route === '/courses') return <CoursesPage />
+        else if (route === '/about') return <AboutPage />
         return <HomePage />
     };
 
